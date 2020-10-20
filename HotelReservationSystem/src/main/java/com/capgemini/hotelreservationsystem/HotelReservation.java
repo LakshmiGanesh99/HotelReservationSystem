721590dacef5f04882ff13e3c4691912519a3b46
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-21 02:34:10 +0530

    [LakshmiGanesh] add:ability to find cheapest hotel for given date range with weekday and weekend rates

--- HotelReservationSystem/src/main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
+++ HotelReservationSystem/src/main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
@@ -1,5 +1,6 @@
 package com.capgemini.hotelreservationsystem;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -7,16 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HotelReservation {
 
-	private static ArrayList<HotelDetails> hotelList = new ArrayList<>();
+	public static ArrayList<HotelDetails> hotelList = new ArrayList<>();
 
 	public ArrayList<HotelDetails> getHotelList() {
 		return hotelList;
 	}
 
-	// UC1
 	public static void addHotel(String name, int weekdayRoomRate, int weekendRoomRate) {
 		HotelDetails temporary = new HotelDetails(name, weekdayRoomRate, weekendRoomRate);
 		hotelList.add(temporary);
@@ -26,15 +27,32 @@ public class HotelReservation {
 		return hotelList.size();
 	}
 
-	// UC2
 	public static String findCheapestHotel(String startDate, String endDate) {
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
 		LocalDate startDateInput = LocalDate.parse(startDate, dateFormat);
 		LocalDate endDateInput = LocalDate.parse(endDate, dateFormat);
 		int noOfDaysToBook = (int) ChronoUnit.DAYS.between(startDateInput, endDateInput) + 1;
-		Map<String, Integer> hotelNameToTotalCostMap = hotelList.stream().collect(Collectors.toMap(hotel -> hotel.getName(), hotel -> hotel.getWeekdayRoomRate() * noOfDaysToBook));
-		String cheapestHotelName = hotelNameToTotalCostMap.keySet().stream().min((hotel1, hotel2) -> hotelNameToTotalCostMap.get(hotel1) - hotelNameToTotalCostMap.get(hotel2)).orElse(null);
-		String cheapestHotelInfo = cheapestHotelName + ", Total Cost: $" + hotelNameToTotalCostMap.get(cheapestHotelName);
+		List<DayOfWeek> daysList = new ArrayList<>();
+		daysList = Stream.iterate(startDateInput.getDayOfWeek(), day -> day.plus(1)).limit(noOfDaysToBook).collect(Collectors.toList());
+		int noOfWeekends = (int) daysList.stream().filter(day -> 
+		day.equals(DayOfWeek.SATURDAY) || day.equals(DayOfWeek.SUNDAY)).count();
+		int noOfWeekdays = daysList.size() - noOfWeekends;
+		int minCost = hotelList.get(0).getWeekdayRoomRate() * noOfWeekdays + hotelList.get(0).getWeekendRoomRate() * noOfWeekends ;
+		List<String> cheapestHotelNameList = new ArrayList<>();
+		cheapestHotelNameList.add(hotelList.get(0).getName());
+		
+		for(int i = 1; i < hotelList.size(); i++) {
+			if(hotelList.get(i).getWeekdayRoomRate() * noOfWeekdays + hotelList.get(i).getWeekendRoomRate() * noOfWeekends < minCost) {
+				minCost = hotelList.get(i).getWeekdayRoomRate() * noOfWeekdays + hotelList.get(i).getWeekendRoomRate() * noOfWeekends;
+				for(int j = 0; j < cheapestHotelNameList.size(); j++) 
+					cheapestHotelNameList.remove(j);
+				cheapestHotelNameList.add(hotelList.get(i).getName());
+			}
+			if(hotelList.get(i).getWeekdayRoomRate() * noOfWeekdays + hotelList.get(i).getWeekendRoomRate() * noOfWeekends == minCost)
+				cheapestHotelNameList.add(hotelList.get(i).getName());
+		}
+		String hotelNameString = cheapestHotelNameList.stream().collect(Collectors.joining(", "));
+		String cheapestHotelInfo = hotelNameString + " Total Cost: $" + minCost;
 		return cheapestHotelInfo;
 	}
-}
+	}
